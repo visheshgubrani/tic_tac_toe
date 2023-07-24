3448d4c89cc40397a30f344d995b80e9ddbd0e9d
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-07-24 19:52:42 +0530

    Initial Commit

--- main.js
+++ main.js
@@ -4,12 +4,26 @@ const startCells = [
     '', '', '', '', '', '', '', '', ''
 ];
 
+let go = 'circle'
+infoDisplay.textContent = 'Circle goes first'
+
 function createBoard() {
-    startCells.forEach((cell, index) => {
+    startCells.forEach((_cell, index) => {
         const cellElement = document.createElement('div');
         cellElement.classList.add('square');
+        cellElement.id = index
+        cellElement.addEventListener('click', addGo)
         gameBoard.append(cellElement)
     })
 }
 
 createBoard()
+
+function addGo(e) {
+    const goDisplay = document.createElement('div');
+    goDisplay.classList.add(go);
+    e.target.append(goDisplay)
+    go = go === 'circle' ? 'cross' : 'circle'
+    infoDisplay.textContent = 'It is now ' + go + 's go.'
+    e.target.removeEventListener('click', addGo)
+}
