fbf3fd5bb6fb8eb0b6f90e73c92cac402174fb6e
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-07-24 20:13:27 +0530

    Initial Commit

--- main.js
+++ main.js
@@ -26,4 +26,36 @@ function addGo(e) {
     go = go === 'circle' ? 'cross' : 'circle'
     infoDisplay.textContent = 'It is now ' + go + 's go.'
     e.target.removeEventListener('click', addGo)
+    checkScore()
+}
+
+function checkScore() {
+    const allSquares = document.querySelectorAll('.square');
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+
+    winningCombos.forEach(array => {
+        const circleWins = array.every(cell =>
+            allSquares[cell].firstChild?.classList.contains('circle'))
+
+        if (circleWins) {
+            infoDisplay.textContent = 'Circle Wins';
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+    })
+
+    winningCombos.forEach(array => {
+        const crossWins = array.every(cell =>
+            allSquares[cell].firstChild?.classList.contains('cross'))
+
+        if (crossWins) {
+            infoDisplay.textContent = 'Cross Wins';
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+    })
 }
