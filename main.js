bc2e233e195af8e80323f88617feebea4274f772
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-07-24 19:36:58 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const gameBoard = document.querySelector('#gameboard');
+const infoDisplay = document.querySelector('#info');
+const startCells = [
+    '', '', '', '', '', '', '', '', ''
+];
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('square');
+        gameBoard.append(cellElement)
+    })
+}
+
+createBoard()
